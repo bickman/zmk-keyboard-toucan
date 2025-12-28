912cac68ae8bfdd47076dc04c46af9f144a3fa48
bickman <bickman@qq.com>
2025-12-28 12:16:57 +0800

    change battery ui

--- boards/shields/nice_view_gem/widgets/battery.c
+++ boards/shields/nice_view_gem/widgets/battery.c
@@ -2,34 +2,41 @@
 #include "battery.h"
 #include "../assets/custom_fonts.h"
 
-LV_IMG_DECLARE(bolt);
-LV_IMG_DECLARE(l_battery_100);
-LV_IMG_DECLARE(l_battery_90);
-LV_IMG_DECLARE(l_battery_75);
-LV_IMG_DECLARE(l_battery_50);
-LV_IMG_DECLARE(l_battery_25);
-LV_IMG_DECLARE(l_battery_10);
+// LV_IMG_DECLARE(bolt);
+// LV_IMG_DECLARE(l_battery_100);
+// LV_IMG_DECLARE(l_battery_90);
+// LV_IMG_DECLARE(l_battery_75);
+// LV_IMG_DECLARE(l_battery_50);
+// LV_IMG_DECLARE(l_battery_25);
+// LV_IMG_DECLARE(l_battery_10);
 
+
+// static void draw_level(lv_obj_t *canvas, const struct status_state *state) {
+//     lv_draw_img_dsc_t img_dsc_l;
+//     lv_draw_img_dsc_init(&img_dsc_l);
+//
+//     uint8_t level = state->battery;
+//     if (level > 90) {
+//         lv_canvas_draw_img(canvas, 8, 8, &l_battery_100, &img_dsc_l);
+//     } else if (level > 75) {
+//         lv_canvas_draw_img(canvas, 8, 8, &l_battery_90, &img_dsc_l);
+//     } else if (level > 50) {
+//         lv_canvas_draw_img(canvas, 8, 8, &l_battery_75, &img_dsc_l);
+//     } else if (level > 25) {
+//         lv_canvas_draw_img(canvas, 8, 8, &l_battery_50, &img_dsc_l);
+//     } else if (level > 10) {
+//         lv_canvas_draw_img(canvas, 8, 8, &l_battery_25, &img_dsc_l);
+//     } else if (level > 1) {
+//         lv_canvas_draw_img(canvas, 8, 8, &l_battery_10, &img_dsc_l);
+//     }
+//
+// }
 
 static void draw_level(lv_obj_t *canvas, const struct status_state *state) {
-    lv_draw_img_dsc_t img_dsc_l;
-    lv_draw_img_dsc_init(&img_dsc_l);
-
+    lv_draw_rect_dsc_t rect_dsc_l;
+    lv_draw_rect_dsc_init(&rect_dsc_l);
     uint8_t level = state->battery;
-    if (level > 90) {
-        lv_canvas_draw_img(canvas, 8, 10, &l_battery_100, &img_dsc_l);
-    } else if (level > 75) {
-        lv_canvas_draw_img(canvas, 8, 10, &l_battery_90, &img_dsc_l);
-    } else if (level > 50) {
-        lv_canvas_draw_img(canvas, 8, 10, &l_battery_75, &img_dsc_l);
-    } else if (level > 25) {
-        lv_canvas_draw_img(canvas, 8, 10, &l_battery_50, &img_dsc_l);
-    } else if (level > 10) {
-        lv_canvas_draw_img(canvas, 8, 10, &l_battery_25, &img_dsc_l);
-    } else if (level > 1) {
-        lv_canvas_draw_img(canvas, 8, 10, &l_battery_10, &img_dsc_l);
-    }
-
+    lv_canvas_draw_rect(canvas, 8, 8, level/2, 3, &rect_dsc_l);
 }
 
 void draw_battery_status(lv_obj_t *canvas, const struct status_state *state) {
--- boards/shields/nice_view_gem/widgets/battery_peripheral.c
+++ boards/shields/nice_view_gem/widgets/battery_peripheral.c
@@ -2,33 +2,40 @@
 #include "battery_peripheral.h"
 #include "../assets/custom_fonts.h"
 
-LV_IMG_DECLARE(bolt);
-LV_IMG_DECLARE(r_battery_100);
-LV_IMG_DECLARE(r_battery_90);
-LV_IMG_DECLARE(r_battery_75);
-LV_IMG_DECLARE(r_battery_50);
-LV_IMG_DECLARE(r_battery_25);
-LV_IMG_DECLARE(r_battery_10);
+// LV_IMG_DECLARE(bolt);
+// LV_IMG_DECLARE(r_battery_100);
+// LV_IMG_DECLARE(r_battery_90);
+// LV_IMG_DECLARE(r_battery_75);
+// LV_IMG_DECLARE(r_battery_50);
+// LV_IMG_DECLARE(r_battery_25);
+// LV_IMG_DECLARE(r_battery_10);
 
+
+// static void draw_level_peripheral(lv_obj_t *canvas, const struct status_state *state) {
+//     lv_draw_img_dsc_t img_dsc_r;
+//     lv_draw_img_dsc_init(&img_dsc_r);
+//
+//     uint8_t level = state->battery_p;
+//     if (level > 90) {
+//         lv_canvas_draw_img(canvas, 80, 10, &r_battery_100, &img_dsc_r);
+//     } else if (level > 75) {
+//         lv_canvas_draw_img(canvas, 80, 10, &r_battery_90, &img_dsc_r);
+//     } else if (level > 50) {
+//         lv_canvas_draw_img(canvas, 80, 10, &r_battery_75, &img_dsc_r);
+//     } else if (level > 25) {
+//         lv_canvas_draw_img(canvas, 80, 10, &r_battery_50, &img_dsc_r);
+//     } else if (level > 10) {
+//         lv_canvas_draw_img(canvas, 80, 10, &r_battery_25, &img_dsc_r);
+//     } else if (level > 1) {
+//         lv_canvas_draw_img(canvas, 80, 10, &r_battery_10, &img_dsc_r);
+//     }
+// }
 
 static void draw_level_peripheral(lv_obj_t *canvas, const struct status_state *state) {
-    lv_draw_img_dsc_t img_dsc_r;
-    lv_draw_img_dsc_init(&img_dsc_r);
-
+    lv_draw_rect_dsc_t rect_dsc_r;
+    lv_draw_rect_dsc_init(&rect_dsc_r);
     uint8_t level = state->battery_p;
-    if (level > 90) {
-        lv_canvas_draw_img(canvas, 80, 10, &r_battery_100, &img_dsc_r);
-    } else if (level > 75) {
-        lv_canvas_draw_img(canvas, 80, 10, &r_battery_90, &img_dsc_r);
-    } else if (level > 50) {
-        lv_canvas_draw_img(canvas, 80, 10, &r_battery_75, &img_dsc_r);
-    } else if (level > 25) {
-        lv_canvas_draw_img(canvas, 80, 10, &r_battery_50, &img_dsc_r);
-    } else if (level > 10) {
-        lv_canvas_draw_img(canvas, 80, 10, &r_battery_25, &img_dsc_r);
-    } else if (level > 1) {
-        lv_canvas_draw_img(canvas, 80, 10, &r_battery_10, &img_dsc_r);
-    }
+    lv_canvas_draw_rect(canvas, 60, 8, level/2, 3, &rect_dsc_r);
 }
 
 void draw_battery_peripheral_status(lv_obj_t *canvas, const struct status_state *state) {
